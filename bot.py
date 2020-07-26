9a9ce8971ee3bc2ba023a0a10b535171bbe0c641
Mukheem <mukheem1603@gmail.com>
2020-07-26 13:42:13 +0530

    added files

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ async def on_message(message):
     if message.content.startswith('hello'):
         await message.channel.send(f'Hey {message.author}!!!')
     if message.content.startswith('help'):
-        await message.channel.send(f'Heyaa {message.author} ❤\nYou can have a look at simple stats of your GitHub profile by a simple command ✨\nThe command: $info yourGitHubusername\n\nHave fun 😁')
+        await message.channel.send(f'Heyaa {message.author.name} ❤\nYou can have a look at simple stats of your GitHub profile by a simple command ✨\nThe command: $info yourGitHubusername\n\nHave fun 😁')
 
 @client.event
 async def on_member_join(member):
